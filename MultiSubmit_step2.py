e3a81cf7482c2906df9c38db54dd315bf748fcec
Deepak Kumar <dekumar@Deepaks-iMac.local>
2020-03-04 12:41:13 +0530

    updated condor Files

--- MultiSubmit_step2.py
+++ MultiSubmit_step2.py
@@ -7,8 +7,8 @@ count=1
 
 def submitjob(count,txtfile):
     #global count
-    submittemp=open("submit_multi_resubmit.sub","w")
-    submitfile=open("submit_multi.sub","r")
+    submittemp=open("submit_multi_temp.sub","w")
+    submitfile=open("submit_multi_step2.sub","r")
     for line in submitfile:
         if line.startswith('transfer_input_files'):
             submittemp.write(line.strip()+', '+txtfile+'\n')
@@ -22,7 +22,7 @@ def submitjob(count,txtfile):
 
     print "\n===============================\nSubmitting jobs #"+str(count)+": "+ txtfile+"\n===============================\n"
 
-    if not test: os.system("condor_submit submit_multi_resubmit.sub")
+    if not test: os.system("condor_submit submit_multi_temp.sub")
 
     #count+=1
 
---
+++ runAnalysis_step2.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#### FRAMEWORK SANDBOX SETUP ####
+# Load cmssw_setup function
+export SCRAM_ARCH=slc6_amd64_gcc700
+source ./cmssw_setup.sh
+
+# Setup CMSSW Base
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+# Download sandbox
+#wget --no-check-certificate "http://stash.osgconnect.net/+ptiwari/sandbox-CMSSW_8_0_26_patch1-76efecd.tar.bz2"
+
+# Setup framework from sandbox
+cmssw_setup sandbox-CMSSW_10_3_0-4cef61e.tar.bz2
+
+cd $CMSSW_BASE
+cmsenv
+cd ../../
+
+python monoHbbAnalyzer.py -F -i "$1"
+
+if [ -e "$4" ]; then
+  until xrdcp -f "$4" root://eoscms.cern.ch//eos/cms/store/group/phys_exotica/monoHiggs/monoHbb/2017_AnalyserOutput/test/"$4"; do 
+    sleep 60
+    echo "Retrying"
+  done
+
+fi
+
+exitcode=$?
+
+if [ ! -e "$4" ]; then
+  echo "Error: The python script failed, could not create the output file."
+  
+fi
+exit $exitcode
+
